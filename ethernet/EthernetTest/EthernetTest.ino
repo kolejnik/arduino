f50cca89b856680d4e43e471f4c1f33bf216c80d
kolejnik <kolejnik@student.umass.edu>
2013-07-16 11:50:43 -0400

    Sending code

---
+++ ethernet/EthernetTest/EthernetTest.ino
@@ -0,0 +1,39 @@
+#include <Ethernet.h>
+#include <SPI.h>
+
+uint32_t i = 0;
+long count = 0;
+
+byte ard_mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x98, 0x19};
+byte ard_ip[] = {192, 168, 7, 2};
+
+EthernetServer server = EthernetServer(59000);
+
+void setup()
+{
+Serial.begin(9600);
+ Ethernet.begin(ard_mac, ard_ip);
+ Serial.println(Ethernet.localIP());
+ 
+ 
+ EthernetClient client;
+ while(!client){
+ client = server.available();
+ Serial.println("Waiting for client...");
+ delay(1000);
+ }
+ 
+ while(client.connected()){
+  server.println(i); 
+  count++;
+ }
+ 
+ Serial.println(count);
+
+}
+
+void loop()
+{
+  
+    
+}

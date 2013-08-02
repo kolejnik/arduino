f0b17493fdf065f54e2111812127ede0574fd7e9
kolejnik <kolejnik@umass.edu>
2013-08-02 16:02:34 -0400

    Comments and whatnot

--- ethernet/EthernetTest/EthernetTest.ino
+++ ethernet/EthernetTest/EthernetTest.ino
@@ -1,11 +1,18 @@
+//Naive Ethernet throughput test using builtin Arduino server.println() 
+//Will send continuously while client is connected. 
+//When client disconnects, prints total values sent. 
+//Use this for debugging and sanity checking.
+//
+//Use with ./ruby/arduinoclient.rb running on host
+
 #include <Ethernet.h>
 #include <SPI.h>
 
-uint32_t i = 0;
-long count = 0;
+uint16_t i = 1234; //Value to send. Real ADC values will be 16-bit
+long count = 0; 
 
 byte ard_mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x98, 0x19};
-byte ard_ip[] = {192, 168, 7, 2};
+byte ard_ip[] = {192, 168, 7, 2}; 
 
 EthernetServer server = EthernetServer(59000);
 

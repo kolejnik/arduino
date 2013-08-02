2268326962c9e63317f60d045bbdaacf44b97e82
kolejnik <kolejnik@umass.edu>
2013-08-02 13:56:47 -0400

    For testing teensy sampler

---
+++ teensy_sampler_test/readSerial.rb
@@ -0,0 +1,33 @@
+    #simplest ruby program to read from arduino serial,
+    #using the SerialPort gem
+    #(http://rubygems.org/gems/serialport)
+     
+    require "serialport"
+     
+    #params for serial port
+    port_str = "COM18"  #may be different for you
+    baud_rate = 9600
+    data_bits = 8
+    stop_bits = 1
+    parity = SerialPort::NONE
+     
+    sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+    f = File.new(ARGV[0], "w");
+	 
+	 
+    #just read forever
+	
+	start = Time.now();
+	while(Time.now() - start < 60)
+		
+			i = sp.gets.chomp       # see note 2
+			#puts i
+			f.write(i);
+			f.write("\n");
+			#puts i.class #String
+			
+		
+     
+	 end
+    sp.close                       #see note 1
+	f.close();
---
+++ teensy_sampler_test/teensy_sampler_test.ino
@@ -0,0 +1,66 @@
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif 
+
+const uint16_t capacity = 128;
+long i = 0;
+uint16_t written = 0;
+uint8_t many[capacity];
+long packetCount = 0;
+void setup(){
+ Serial.begin(9600);  
+ 
+ //PRADC -- enable clock. It doesn't like this so I left it out
+//cbi(PRR, PRADC); 
+
+//Prescaler: 16
+sbi(ADCSRA, ADPS2);
+cbi(ADCSRA, ADPS1);
+cbi(ADCSRA, ADPS0);
+
+//Free-running mode
+cbi(ADCSRB, ADTS0);
+cbi(ADCSRB, ADTS1);
+cbi(ADCSRB, ADTS2);
+
+//Source: free-running
+sbi(ADCSRA, ADATE);
+
+//ADC0
+cbi(ADMUX, MUX0);
+cbi(ADMUX, MUX1);
+cbi(ADMUX, MUX2);
+cbi(ADMUX, MUX3);
+
+//Avcc -- Internal 5V reference
+cbi(ADMUX, REFS1);
+sbi(ADMUX, REFS0);
+
+//Disable digital input buffer, save power
+//sbi(DIDR0,ADC0D);
+
+sbi(ADCSRA, ADEN); //Enable
+sbi(ADCSRA, ADSC); //Initial conversion is slow
+
+}
+
+void loop(){
+  
+     
+     while (!(bit_is_set(ADCSRA, ADIF))){
+    //Serial.println("Waiting!");
+  }
+        
+  uint8_t lo = ADCL;
+  uint8_t hi = ADCH;
+  Serial.println(lo | (hi << 8));
+  delay(100);
+
+   //Reset ADIF for next conversion by setting it to 1.
+   sbi(ADCSRA, ADIF);
+  }
+  
+

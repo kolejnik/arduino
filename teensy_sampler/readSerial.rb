dbde65fd2d95717f069eb5068ccb4137fe58b71a
kolejnik <kolejnik@umass.edu>
2013-08-08 15:59:43 -0400

    Parse while reading from serial port

--- teensy_sampler/readSerial.rb
+++ teensy_sampler/readSerial.rb
@@ -7,12 +7,13 @@
     require "serialport"
      
     #params for serial port
-    port_str = "/dev/ttyACM0"  #may be different for you
+    port_str = "COM18"  #may be different for you
     baud_rate = 9600
     data_bits = 8
     stop_bits = 1
     parity = SerialPort::NONE
      
+	 values_read = 0;
     sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
     f = File.new(ARGV[0], "w");
     sp.read_timeout = 8; 
@@ -22,14 +23,24 @@
 	start = Time.now();
 	while(Time.now() - start < 60)
 		
-			i = sp.gets       # see note 2
-			#puts i
-			f.write(i);
+			stuff = sp.read(2)       # see note 2
+			while(stuff == nil)
+			stuff = sp.read(2)
+			end
+			
+			values_read += 1;
+			stuff = stuff.bytes.to_a
+			result = stuff[0] | (stuff[1] << 8)
+			f.write(result);
 			f.write("\n");
-			#puts i.class #String
+			#puts stuff;
+			
+			#puts stuff.class #String
 			
 		
      
 	 end
     sp.close                       #see note 1
 	f.close();
+	
+	puts (values_read/60.0);
